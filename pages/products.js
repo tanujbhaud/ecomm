d2a226a4ece44a29ef471acf50da46d516be818a
Tanuj Bhaud <tanujbhaud@gmail.com>
2023-08-09 13:07:35 +0530

    changed product

--- pages/products.js
+++ pages/products.js
@@ -8,7 +8,7 @@ export default function Products() {
   useEffect(() => {
     axios
       .post("https://awstest.sperentes.com/ws/girish/get-product-list", {
-        api_key: process.env.api_key,
+        api_key: "8cfb43a5-54f1-4396-adb3-ff6718b687ec",
       })
       .then(function (response) {
         console.log("this is the res", response.data);
